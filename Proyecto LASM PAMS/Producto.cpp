b4dab96375abf9f2ed79521d3ff1c5edaaf0932c
Laurita05 <lauxyz1234@gmail.com>
2024-11-15 18:16:25 -0600

    Otra
    Que me pase de wey me dijo el codigo

--- Proyecto LASM PAMS/Producto.cpp
+++ Proyecto LASM PAMS/Producto.cpp
@@ -1,11 +1,11 @@
 #include "Producto.h"
 
-friend ostream& operator<<(ostream &salida, const Producto &obj)
+ostream& operator<<(ostream &salida, const Producto &obj)
 {
     salida<<"Nombre: "<<obj.nombre<<", Precio: $"<<obj.precio<<endl;
     return salida;
 }
-friend istream& operator>>(istream &entrada, Producto &obj)
+istream& operator>>(istream &entrada, Producto &obj)
 {
     cout<<"Ingrese el nuevo nombre del producto: "<<endl;
     entrada>>obj.nombre;
@@ -17,30 +17,35 @@ Producto Producto::operator+(const Producto &obj)
 {
     Producto aux;
     aux.nombre=nombre+" + "+obj.nombre;
-    aux.precio=precio+" + "+obj.precio;
+    aux.precio=precio+obj.precio;
     return aux;
 }
 Producto Producto::operator-(const Producto &obj)
 {
     Producto aux;
     aux.nombre=nombre+" - "+obj.nombre;
-    aux.precio=precio+" - "+obj.precio;
+    aux.precio=precio+obj.precio;
     return aux;
 }
 Producto Producto::operator*(const Producto &obj)
 {
     Producto aux;
     aux.nombre=nombre+" x "+obj.nombre;
-    aux.precio=precio+" x "+obj.precio;
+    aux.precio=precio+obj.precio;
     return aux;
 }
 Producto Producto::operator/(const Producto &obj)
 {
     Producto aux;
     aux.nombre=nombre+" / "+obj.nombre;
-    aux.precio=precio+" / "+obj.precio;
+    aux.precio=precio+obj.precio;
     return aux;
 }
+/*Producto Producto::operator=(const Producto &obj);
+bool Producto::operator==(const Producto &obj)
+{
+    return (nombre == other.nombre && precio == other.precio);
+}*/
 //-----------------------------------------------------
 Producto::Producto()
 {
